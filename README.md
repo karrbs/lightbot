95c488a0e58fcf4b1d405af436ff0c302649ce4c
Karlan Bousquet <Karlan@karlans-mbp.attlocal.net>
2017-03-19 18:09:02 -0500

    Syncing Git with New
    This is to update the Old git with the new files to be able to work
    with latest code.

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# lightbot
+# lightlybot
+Lightly Bot allows the stream to have the viewers of their stream vote on the light color of the streamers Phillip hue lighting.
+
+
--- Services/twitch_api/twitch.js
+++ Services/twitch_api/twitch.js
@@ -3,10 +3,40 @@ var http = require('http');
 var _ = require('lodash');
 var S = require('string');
 var config = require('config');
+var util = require('util');
 
 
 
 var clientID = config.get('twitchAPI.clientID');
-var scope = config.get('twitchAPI.scope');
+
+/*https://api.twitch.tv/kraken/oauth2/authorize
+    ?response_type=code
+    &client_id=[your client ID]
+    &redirect_uri=[your registered redirect URI]
+    &scope=[space separated list of scopes]
+    &state=[your provided unique token]*/
 
 
+
+
+var createGetAuth = function(options, res) {
+
+    var baseUrl = "https://api.twitch.tv/kraken/oauth2/authorize?response_type=code&client_id=";
+    var redirectURL = "&redirect_uri=";
+    var scope = "&scope=";
+    var state = "&state="; //[your provided unique token]
+
+    var fullURL = util.format('%s%s%s%s%s%s%s', baseUrl,clientID,redirectURL,"http://localhost",scope,'chat_login',state,'dev');
+
+    console.log(fullURL);
+    request.get(options, function (error, response, body) {
+        if (error) {
+            return console.error('upload failed:', error);
+        }
+        console.log(response.statusCode);
+        console.log('Upload successful!  Server responded with:', body);
+        res(body);
+    });
+};
+
+createGetAuth();
